01430d5b796917db4f668515b39874701d4b7785
Renato Yufra Tozo <renatoyufra@gmail.com>
2022-04-02 20:57:52 -0500

    [feat] Create navbar

---
+++ components/Banner.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="h-96 bg-blue-500 flex justify-center items-center">
+      <div>
+          Banner
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "BannerComponent"
+}
+</script>
+
+<style>
+
+</style>
---
+++ components/Menu.vue
@@ -0,0 +1,65 @@
+<template>
+    <nav class="bg-white relative z-10">
+        <div class="relative my-0 px-10 flex justify-between items-center">
+            <a href="#" class="logo">
+                <img src="~/assets/images/logoA.png" alt="Elohim">
+            </a>
+            <ul class="flex w-full justify-evenly py-5 font-bold">
+                <li v-for="(item, index) in menu" :key="index" class="uppercase">{{ item.name }}</li>
+            </ul>
+            <ul class="w-48 flex justify-evenly py-2">
+                <li class="flex items-center">
+                    <a id="esp" href="#"></a>
+                </li>
+                <li class="flex items-center">
+                    <a id="eng" href="#"></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'MegaMenu',
+    data () {
+        return {
+            menu: [
+                { name: 'Inicio' },
+                { name: '¿Quienes Somos?' },
+                { name: 'Catálogo' },
+                { name: 'Encuéntranos' },
+                { name: 'Mundo' },
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+.logo {
+    position: absolute;
+    width: 129px;
+    height: 108px;
+    top: -20px;
+    left: 46%;
+}
+
+nav ul li:nth-child(3) {
+    margin-right: 250px;
+}
+
+#esp {
+    width: 35px;
+    height: 35px;
+    display: inline-block;
+    background-image: url("~/assets/images/ESP.svg");
+}
+#eng {
+    width: 35px;
+    height: 35px;
+    display: inline-block;
+    background-image: url("~/assets/images/ENG.svg");
+}
+</style>
+
